e35adab69d077dd70de63e8d0b3b5b0e56f58216
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-03-02 18:52:32 +0900

    Add files via upload

---
+++ baekjoon:C++/무제 폴더/202403/20240302.cpp
@@ -0,0 +1,24 @@
+/*1
+
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a , b , num = 0;
+    cin >> a >> b;
+    for (int i = 1; i <= a; i++) {
+        if (a % i == 0) {
+            num += 1;
+            if (num == b) {
+                cout << i;
+            }
+        }
+    }
+    if (num < b) {
+        cout << 0;
+    }
+}
