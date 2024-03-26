04c6d047ab1119bb01dce0bbd9887764b424b2e5
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-03-26 15:20:01 +0900

    Add files via upload

---
+++ 백준 알고리즘/JAVA/202403/_240326_.java
@@ -0,0 +1,31 @@
+/*1
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //방정식 계수
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        int ans_x = 0;
+        int ans_y = 0;
+        for (int x = -999; x < 1000; x++) {
+            for (int y = -999; y < 1000; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    ans_x = x;
+                    ans_y = y;
+                }
+            }
+        }
+        System.out.println(ans_x + " " + ans_y);
+    }
+}
+ */
+
+/*2
+
+ */
