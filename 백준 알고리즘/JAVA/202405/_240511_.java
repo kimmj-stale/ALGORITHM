0dc204e6dc133f688b03de834638ad2830d11be8
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-05-13 23:19:53 +0900

    Update _240511_.java

--- 백준 알고리즘/JAVA/202405/_240511_.java
+++ 백준 알고리즘/JAVA/202405/_240511_.java
@@ -7,6 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        
         while (true) {
             Stack<Integer> table = new Stack<>();
             String p = br.readLine();
@@ -80,7 +81,7 @@ public class Main {
         int cnt = 1;
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine() , " ");
-        // 52413
+        
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
             if(num != cnt) table_left.push(num);
