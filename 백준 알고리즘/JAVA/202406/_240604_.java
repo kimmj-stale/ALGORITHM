fe475bd95c2871a897ccf8ad38f183ee8b1abb53
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-06-24 23:25:05 +0900

    Add files via upload

---
+++ 백준 알고리즘/JAVA/202406/_240604_.java
@@ -0,0 +1,54 @@
+/*1
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static long LCM(long a , long b) {
+        long r;
+        long org_a = a;
+        long org_b = b;
+        if (a < b) {
+            long tmp = a;
+            a = b;
+            b = tmp;
+        }
+        while (b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return (org_a * org_b) / a;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine() , " ");
+        int initial = Integer.parseInt(st.nextToken());
+
+        int[] table = new int[n];
+        table[0] = initial;
+        long val = initial;
+        for (int i = 1; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (initial > a) initial = a;
+            table[i] = a;
+            val = LCM(val , a);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (table[i] == val) {
+                val *= initial;
+                break;
+            }
+        }
+        bw.write(val + "");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+ */
+
---
+++ 백준 알고리즘/JAVA/202406/_240624_.java
@@ -0,0 +1,37 @@
+/*1
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        HashMap<String , Integer> table = new HashMap<>();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String nickname = st.nextToken();
+            if (nickname.equals("ENTER")) {
+                table.clear();
+            }
+            else {
+                if (!table.containsKey(nickname)) {
+                    ans++;
+                }
+                else table.put(nickname , 0);
+            }
+            table.put(nickname , 0);
+        }
+        br.close();
+        bw.write(ans + "");
+        bw.flush();
+        bw.close();
+    }
+}
+ */
