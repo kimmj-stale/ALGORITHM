a8311c084c8b9f3c06b1c57e8422864fa28d3d7e
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-07-01 21:32:21 +0900

    Update _240629_.java

--- 백준 알고리즘/JAVA/202406/_240629_.java
+++ 백준 알고리즘/JAVA/202406/_240629_.java
@@ -11,11 +11,11 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         HashMap<String , Integer> table = new HashMap<>();
         int ans = 0;
+        
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             String nick1 = st.nextToken();
             String nick2 = st.nextToken();
-
             if (!table.containsKey(nick1)) table.put(nick1 , 0);
             if (!table.containsKey(nick2)) table.put(nick2 , 0);
 
