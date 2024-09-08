ff31957e3b7b4f57a25068e1ed79bbab8e09d2c6
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-09-08 16:24:13 +0900

    Update _240905_.java

--- 백준 알고리즘/JAVA/202409/_240905_.java
+++ 백준 알고리즘/JAVA/202409/_240905_.java
@@ -1,1 +1,43 @@
 /*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                int tempRow = x;
+                int tempColumn = y;
+                int tempN = n;
+
+                if (tempRow%3 == 1 && tempColumn%3 == 1) {
+                    sb.append(' ');
+                } else {
+                    while (true) {
+                        tempRow %= tempN;
+                        tempColumn %= tempN;
+
+                        tempN /= 3;
+                        if (tempRow < 3 || tempColumn < 3) {
+                            sb.append('*');
+                            break;
+                        }
+
+                        if ((tempN * 2 - 1 >= tempRow && tempRow >= tempN) && (tempN * 2 - 1 >= tempColumn && tempColumn >= tempN)) {
+                            sb.append(" ");
+                            break;
+                        }
+                    }
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+*/
