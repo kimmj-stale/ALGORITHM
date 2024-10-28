78d9eee567f1a5a272afacbbb7fc7462a1ca3497
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-10-28 15:58:51 +0900

    Update _241012.java

--- 백준 알고리즘/JAVA/202410/_241012.java
+++ 백준 알고리즘/JAVA/202410/_241012.java
@@ -1,3 +1,23 @@
-/* 백준 5598 - 카이스라 암호
+/* 백준 5598 - 카이사르 암호
+import java.io.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String caesar_word = br.readLine();
+        String ans = "";
+        for (int i = 0; i < caesar_word.length(); i++) {
+            char text = caesar_word.charAt(i);
+            if (text >= 68) text -= 3;
+            else text += 23;
+            ans += text;
+        }
+        bw.write(ans + "");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
  */
