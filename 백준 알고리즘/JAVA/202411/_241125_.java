4a76b18b044380a3d814dc778b645b857394d145
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-11-26 14:54:48 +0900

    Update _241125_.java

--- 백준 알고리즘/JAVA/202411/_241125_.java
+++ 백준 알고리즘/JAVA/202411/_241125_.java
@@ -40,3 +40,47 @@ public class Main {
     }
 }
  */
+
+/* 백준 1654 - 전선 자르기
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n , m;
+    static int[] cable;
+    static long min , max , mid;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        cable = new int[n];
+        for (int i = 0; i < n; i++) {
+            cable[i] = Integer.parseInt(br.readLine());
+        }
+        //이분탐색 알고리즘 사용
+        min = 1L;
+        mid = 0L;
+        max = Long.valueOf(Arrays.stream(cable).max().getAsInt());
+
+        while (true) {
+            int sum = 0;
+            mid = (min + max) / 2;
+            for (int i = 0; i < cable.length; i++) {
+                sum += cable[i] / mid;
+            }
+
+            if (min > max) break;
+            else if (sum >= m) min = mid + 1;
+            else max = mid - 1;
+        }
+        bw.write(max + "");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
+ */
