d95290e042fdee7afe11420c66d62d78e7d854b4
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2025-03-02 12:17:06 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n , m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        DFS(0 , 0);
+
+        bw.write(sb + "");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    static void DFS(int depth , int k) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = k; i < n; i++) {
+            arr[depth] = i + 1;
+            DFS(depth + 1 , i);
+        }
+    }
+}
