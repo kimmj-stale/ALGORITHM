7d9f8f6b3594cd5bc83230728010ca2f3180adf9
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2025-03-19 13:07:11 +0900

    [Silver IV] Title: 수 찾기, Time: 536 ms, Memory: 48928 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,28 +1,25 @@
-//hash map을 쓰는 방식이 더 효율적으로 빠름
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n , m;
-    static Map<Integer , Integer> comp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-
-        n = Integer.parseInt(br.readLine());
-        comp = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
+        //Hash map에 담는 이유
+            //hash map의 탐색 알고리즘은 merge sort보다 좋은 알고리즘을 사용하므로 map을 사용하는 편이 좋다.
+        Map<Integer , Integer> comp = new HashMap<>();
         st = new StringTokenizer(br.readLine() , " ");
         for (int i = 0; i < n; i++) {
             comp.put(Integer.parseInt(st.nextToken()) , 0);
         }
 
-        m = Integer.parseInt(br.readLine());
-        int val = 0;
+        int m = Integer.parseInt(br.readLine());
+        int val;
         st = new StringTokenizer(br.readLine() , " ");
         for (int i = 0; i < m; i++) {
             val = Integer.parseInt(st.nextToken());
@@ -30,6 +27,7 @@ public class Main {
             else bw.write(0 + "\n");
         }
 
+        //StringBuilder에 담아서 출력하는 경우가 더 느림
         bw.flush();
         br.close();
         bw.close();
